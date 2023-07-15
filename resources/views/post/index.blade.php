65dfa5e73a78d8b197e982baef41478cb914e3a4
Anna1808v <voskoboyeva18@gmail.com>
2023-07-15 20:29:19 +0300

    pagination

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -6,15 +6,43 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>All posts</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    <container>
-        <div> <h1>All posts</h1></div>
-        <div>
-            @foreach($posts as $post)
-                <div>{{ $post->id }} . {{ $post->title }}</div>
-            @endforeach
-        </div>
-    </container>
+    <div class="container">
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('main') }}">Main</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+
+        <div class="row">
+
+            <div>
+                <h2>All posts</h2>
+            </div>
+
+            <div>
+            </div>
+            <table class="table">
+                <tbody>
+                @foreach($posts as $post)
+                    <tr>
+                        <th scope="row">{{ $post->id }}</th>
+                        <td>{{ $post->title }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            <div class="mt-3">
+                {{ $posts->links() }}
+            </div>
+    </div>
 </body>
 </html>
